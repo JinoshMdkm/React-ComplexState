005272dbf9e8ab89a5a10fbca5513b107cf44edd
JinoshMdkm <jinoshkurisinkal@hotmail.com>
2021-09-12 23:17:09 +0530

    inputOnChange

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [first, setFirst] = useState("");
+  const [second, setSecond] = useState("");
+  function setText() {}
+  function getFirstName(event) {
+    setFirst(event.target.value);
+  }
+  function getSecond(event) {
+    setSecond(event.target.value);
+  }
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>
+        Hello {first} {second}
+      </h1>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
-        <button>Submit</button>
+        <input name="fName" placeholder="First Name" onChange={getFirstName} />
+        <input name="lName" placeholder="Last Name" onChange={getSecond} />
+        <button onClick={setText}>Submit</button>
       </form>
     </div>
   );
